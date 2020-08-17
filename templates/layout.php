7498f0dc70c5e4933a5f4c24df20e1d61f0b69fe
Sergik123 <phoenix925012@yandex.ru>
2020-08-17 16:36:04 +0300

    Реализация header,footer,layout,navigation

--- templates/layout.php
+++ templates/layout.php
@@ -1,15 +1,12 @@
 <?php
-echo '<div class="list-group">
-    <a href="#" class="list-group-item">
-        <h4 class="list-group-item-heading">Что такое HTML?</h4>
-        <p class="list-group-item-text">HTML - это стандартный язык разметки документов во Всемирной паутине.</p>
-    </a>
-    <a href="#" class="list-group-item active">
-        <h4 class="list-group-item-heading">Что такое CSS?</h4>
-        <p class="list-group-item-text">CSS - это формальный язык описания внешнего вида документа, написанного с использованием языка разметки.</p>
-    </a>
-    <a href="#" class="list-group-item">
-        <h4 class="list-group-item-heading">Что такое JavaScript?</h4>
-        <p class="list-group-item-text">JavaScript - это прототипно-ориентированный сценарный язык программирования. Является диалектом языка ECMAScript.</p>
-    </a>
-</div>';
+require_once ('../db/post_list.php');
+
+echo '<div class="list-group">';
+
+foreach ($posts as $post){
+    echo ' <a href="#" class="list-group-item '.$post['active'].'">';
+    echo ' <h4 class="list-group-item-heading">'.$post['title'].'</h4>';
+    echo ' <p class="list-group-item-text">'.$post['description'].'</p>';
+    echo '</a>';
+}
+echo '</div>';
